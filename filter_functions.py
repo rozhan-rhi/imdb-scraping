576f17dae17c92c3e8a0e087594fa7b771331a67
rozhan-rhi <rojanrahmani2002@yahoo.com>
2021-10-16 22:11:42 +0330

    country filter,adualt option and comments added

--- filter_functions.py
+++ filter_functions.py
@@ -3,36 +3,57 @@ import requests
 from links import Link_Base
 
 class Filters_Sub(Filter_Base,Link_Base) :
+    """finds movies based on filters"""
 
-    def common_part(self,func) :
-        obj.parse_page(func)
-        self.total_info=obj.several_movie(obj.main_page,obj.movie_detail)
+    def common_part(self,url) :
+        """parse the url of function in parameter and use several_movie method to return the dict of movies and their details
+        it's common among filters except movie_name_filter"""
+        obj.parse_page(url)    #parse url in parameter
+        self.total_info=obj.several_movie(obj.main_page,obj.movie_detail) #use several_movie method to find movies' names and  their contents 
         return self.total_info
+
     
-    def rating_filter(self,start,end) :
-        self.url=Link_Base(rating_start=start,rating_end=end).rating_url
+    def rating_filter(self,start,end,adult=None) :
+        """find movies based on rating . it gets three parameters, first two parameters are range of rating and last one is adult limit """
+        self.url=Link_Base(rating_start=start,rating_end=end,adult_limit=adult).rating_url  #get the rating url from links.py
         return obj.common_part(self.url)
+
 
     def movie_name_filter(self,name):
-        self.url=Link_Base(movie_name=name).movie_name_url
-        obj.parse_page(self.url)
-        self.total_info=obj.one_movie(obj.main_page,obj.movie_detail)
+        """find all information about the movie by its name"""
+        self.url=Link_Base(movie_name=name).movie_name_url #get the movie_name url from links.py
+        obj.parse_page(self.url) #parse url 
+        self.total_info=obj.one_movie(obj.main_page,obj.movie_detail)   #use one_movie method to find movie's name and its content
         return self.total_info
 
-    def genre_filter(self,genre_name):
-        self.url=Link_Base(genre_name=genre_name).genre_url
+
+    def genre_filter(self,genre_name,adult=None):
+        """find movies based on their generes"""
+        self.url=Link_Base(genre_name=genre_name,adult_limit=adult).genre_url   #get the genre url from links.py
         return obj.common_part(self.url)
 
-    def release_date_filter(self,start,end):
-        self.url=Link_Base(date_start=start,date_end=end).release_url
+
+    def release_date_filter(self,start,end,adult=None):
+        """find movies based on their release dates.2 first parameters are the range of dates"""
+        self.url=Link_Base(date_start=start,date_end=end,adult_limit=adult).release_url    #get the release date url from links.py
+        return obj.common_part(self.url)
+
+
+    def country_filter(self,country_name,adult=None) :
+        """find movies based on the country movies produced"""
+        self.base_url=Link_Base().advanced_url      #get the country url from links.p
+        obj.parse_page(self.base_url)
+        self.page_labels=self.parse.find_all("div",class_="clause") #find all parts of advanced search page
+        for self.label in self.page_labels:
+            if self.label.h3.text=="Countries":     #find countries label
+                self.country_options=self.label.find_all("option")     #find all countries option
+                for self.each_option in self.country_options:
+                    if self.each_option.text.lower()==country_name :    #find the country that match with country_name in parameter
+                        self.country_brief=self.each_option["value"]    #find the abbreviation of the name of country
+        self.url=Link_Base(country_name=self.country_brief,adult_limit=adult).country_url
+        # print(self.url)
         return obj.common_part(self.url)
 
 
 obj=Filters_Sub()
-# obj_link=Link_Base()
-# obj_filter_base=Filter_Base()
-# print(obj.rating_filter("7","8"))
-print(obj.release_date_filter("2020-01-01","2020-12-12"))
-
-# x=obj.parse_page(obj.rating("7.0","7.8"))
-# print(obj.several_movie(obj.main_page,obj.movie_detail))
+print(obj.release_date_filter("2020-01-01","2020-12-12",adult="include"))
