c2b1532a5dc1bdd21e93658ef0ddd85981f19ad9
Rozhin81 <79437690+Rozhin81@users.noreply.github.com>
2021-10-21 18:28:15 +0330

    changes added

--- links.py
+++ links.py
@@ -1,5 +1,5 @@
 class Link_Base:
-    """maintain all link"""
+    """maintains all links"""
     def __init__(self,personal_link=None,rating_start=None,rating_end=None,movie_name=None,genre_name=None,date_start=None,date_end=None,adult_limit=None,country_name=None):
         self.main_page_url=f"https://www.imdb.com{personal_link}"
         self.rating_url=f"https://www.imdb.com/search/title/?user_rating={rating_start},{rating_end}&adult={adult_limit}"
