800a96a60beedcbfc6e09aea72956713d6815f66
rozhan-rhi <rojanrahmani2002@yahoo.com>
2021-10-30 10:59:17 +0330

    callbacks added

--- find_movies.py
+++ find_movies.py
@@ -1,9 +1,11 @@
 from filter_basic import Filter_Base
-
+from filter_funcs_callback import Filters_Callback
 class Movies(Filter_Base):
-    def several_movie(self,url):
+    
+    def several_movie(self,callback):
         """gets 2 functions as parameter and finds all contents of some movies"""
-        self.list_movies=super().parse_page(url).find_all("div",class_="lister-item mode-advanced") #finds all movies in a page
+        self.url=callback
+        self.list_movies=super().parse_page(self.url).find_all("div",class_="lister-item mode-advanced") #finds all movies in a page
         for self.each_movie in self.list_movies :
             self.movie_content=self.each_movie.find("div",class_="lister-item-content") #finds each movie part
             self.total_content=super().movie_detail(super().main_page(self.movie_content)) #uses 2 functions in parameter to finds all contents of movies
@@ -11,9 +13,10 @@ class Movies(Filter_Base):
 
 
 
-    def one_movie(self,url):
+    def one_movie(self,callback):
         """gets 2 functions as parameter and finds all contents of one movie"""
-        self.movie=super().parse_page(url).find("div",class_="lister-item mode-advanced")    #finds first movie
+        self.url=callback
+        self.movie=super().parse_page(self.url).find("div",class_="lister-item mode-advanced")    #finds first movie
         self.movie_content=self.movie.find("div",class_="lister-item-content")  #finds the movie part
         self.total_content=super().movie_detail(super().main_page(self.movie_content))  #uses 2 functions in parameter to finds all contents of movie
         return  self.total_content
