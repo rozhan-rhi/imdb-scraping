52afce01225a3d85ce7a307d702ad4cf73cd5b40
Rozhin81 <79437690+Rozhin81@users.noreply.github.com>
2021-11-28 23:40:30 +0330

    replace files into folders

--- human_search/links_human.py
+++
@@ -1,6 +0,0 @@
-class Links :
-    """includes all links"""
-    def __init__(self,person_name=None,special_link=None) :
-        self.person_url=f"https://www.imdb.com/search/name/?name={person_name}"
-        self.bio_url=f"https://www.imdb.com{special_link}/bio?ref_=nm_ov_bio_sm"
-        self.work_url=f"https://www.imdb.com{special_link}/?ref_=tt_ov_st"
