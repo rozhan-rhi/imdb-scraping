2cf476c13b8ee1f1b14dc7213e294fab1ceb48ed
Rozhin81 <79437690+Rozhin81@users.noreply.github.com>
2022-01-22 22:03:47 +0330

    some changes in older code added

--- filter_search/information/story_line.py
+++ filter_search/information/story_line.py
@@ -2,38 +2,54 @@ import requests
 from bs4 import BeautifulSoup
 from ..total_links import Link_Base
 from .filter_basic import Filter_Base
+import re
+
 class Movie_Storyline(Filter_Base):
     
-    def __init__(self,pasre_url,p_link):
-        self.pasre_url=pasre_url
+    def __init__(self,parse_url,p_link):
+        self.parse_url=parse_url
         self.p_link=p_link
     
     
     def genre(self):
-        self.find_genre=self.pasre_url.find("li",{"data-testid":"storyline-genres"}) #finds genre part
-        self.genre_label=self.find_genre.span.text  #finds title(genre)
-        self.genre_content=[self.item.a.text for self.item in self.find_genre.find_all("li",role="presentation")]   #finds all genre types of movie
+        # self.find_genre=self.pasre_url.find("li",{"data-testid":"storyline-genres"}) #finds genre part
+        # self.genre_label=self.find_genre.span.text  #finds title(genre)
+        self.genre_content=[self.item.text for self.item in self.parse_url.find_all("a",href=True) if "search" in self.item['href'].split("/") and "?genres" in re.split('/|=',self.item['href'])]   #finds all genre types of movie
         return self.genre_content
         
         
     def summary(self):
-        self.summary_part=self.pasre_url.find("section",{"data-testid":"Storyline"}).find("div",class_="ipc-html-content ipc-html-content--base").div.text
-        return [self.summary_part]
-    
+        try :
+            self.summary_part=self.parse_url.find("section",{"data-testid":"Storyline"}).find("div",class_="ipc-html-content ipc-html-content--base").div.text.strip()
+            return [self.summary_part]
+        except :pass
     
    
     def movie_tag(self):
-        self.tag_page=Link_Base(tagline=self.p_link).tagline
-        self.parse=super().parse_page(self.tag_page)
-        self.tag_part=self.parse.find("div",id="taglines_content")
-        self.no_tag=self.tag_part.find("div",id="no_content")
-        if self.no_tag==None:
-            self.all_tags=self.tag_part.find_all("div",class_="soda")
-            self.tag_content=[self.each_tag.text.strip().replace("\n","") for self.each_tag in self.all_tags]
-            return self.tag_content
-        else:
-            return "It looks like we don\'t have any Taglines for this title yet"
+        try :
+            self.link_tag=self.parse_url.find("a",href=True) 
+            if "taglines" in self.link_tag['href'] :
+                self.line_names=["odd","even"]
+                for self.each_line_name in self.line_names :
+                    self.tagline_page=[self.tagline.text for self.tagline in self.parse_page(f"{'https://www.imdb.com'}{self.link_tag['href']}").find_all("div",class_=f"soda {self.each_line_name}")]
+                    return self.tagline_page
+        except:
+            self.tagline_page=self.parse_url.find("span",class_="ipc-metadata-list-item__list-content-item").text 
+            return self.tagline_page
+        finally :
+            return self.tagline_page
+            
         
+        # self.tag_link=Link_Base(tagline=self.p_link).tagline
+        # self.parse=super().parse_page(self.tag_link)
+        # self.tag_part=self.parse.find("div",id="taglines_content")
+        # try :
+        #     self.all_tags=self.tag_part.find_all("div")
+        #     self.tag_content=[self.each_tag.text.strip().replace("\n","") for self.each_tag in self.all_tags]
+        # except :
+        #     self.tag_content="no tagline"
+        # finally :
+        #     return self.tag_content
         
         
     def movie_parents_guide(self):
--- filter_search/movies_based_information/saving_info.py
+++ filter_search/movies_based_information/saving_info.py
@@ -10,37 +10,37 @@ class Save_Info(Filter_Base):
     """save all information about movie """
 
     
-    def movie_name(self,parse_text):
-        self.finding_name=parse_text
-        self.name=self.finding_name.h3.a.text #finds the name of movie
-        return self.name
+    # def movie_name(self,parse_text):
+    #     self.finding_name=parse_text
+    #     self.name=self.finding_name.h3.a.text #finds the name of movie
+    #     return self.name
     
-    def calling_classes(self,base_page):
+    def calling_classes(self,url,personal):
         self.movie_specs={}  #save all movies with their details  
-        self.page=super().main_page(base_page)
-        self.special_part=super().get_personal_Link
-        self.obj_details=Movie_Details(self.page)
-        self.obj_participant=Movie_Participant(self.page)
-        self.obj_rating=Movie_Rating(self.page)
-        self.obj_runtime=Movie_Runtime(self.page)
-        self.obj_storyline=Movie_Storyline(pasre_url=self.page,p_link=self.special_part)
-        self.obj_opinion_award=Awards(self.special_part)
-        self.obj_opinion_review=User_Reviews(self.special_part)
-        self.obj_opinion_rating=User_Rating(self.special_part)
+        self.personal=personal
+        self.page=super().parse_page(url)
+        # self.special_part=super().get_personal_Link
+        # self.obj_details=Movie_Details(self.page)
+        # self.obj_participant=Movie_Participant(self.page)
+        # self.obj_rating=Movie_Rating(self.page)
+        # self.obj_runtime=Movie_Runtime(self.page)
+        self.obj_storyline=Movie_Storyline(parse_url=self.page,p_link=self.personal)
+        # self.obj_opinion_award=Awards(self.personal)
+        # self.obj_opinion_review=User_Reviews(self.personal)
+        # self.obj_opinion_rating=User_Rating(self.personal)
         
         
-        self.movie_specs["runtime"]=self.obj_runtime.runtime()
-        self.movie_specs["genre"]=self.obj_storyline.genre()
-        self.movie_specs["rating"]=self.obj_rating.rating()
+        # self.movie_specs["runtime"]=self.obj_runtime.runtime()
+        # self.movie_specs["genre"]=self.obj_storyline.genre()
+        # self.movie_specs["rating"]=self.obj_rating.rating()
         self.movie_specs["summary"]=self.obj_storyline.summary()
-        self.movie_specs["movie_tag"]=self.obj_storyline.movie_tag()
-        self.movie_specs["participant"]=self.obj_participant.people()
-        self.movie_specs["detail"]=self.obj_details.detail_part()
-        self.movie_specs["parent_guide"]=self.obj_storyline.movie_parents_guide()
-        self.movie_specs["awards"]=self.obj_opinion_award.awards_page()
-        self.movie_specs["user-reviews"]=self.obj_opinion_review.review()
-        self.movie_specs["user-rating"]=self.obj_opinion_rating.rating_movie()
-        self.movie_specs["user-reviews"]=self.obj_opinion_review.review()
+        # self.movie_specs["movie_tag"]=self.obj_storyline.movie_tag()
+        # self.movie_specs["participant"]=self.obj_participant.people()
+        # self.movie_specs["detail"]=self.obj_details.detail_part()
+        # self.movie_specs["parent_guide"]=self.obj_storyline.movie_parents_guide()
+        # self.movie_specs["awards"]=self.obj_opinion_award.awards_page()
+        # self.movie_specs["user-reviews"]=self.obj_opinion_review.review()
+        # self.movie_specs["user-rating"]=self.obj_opinion_rating.rating_movie()
 
         return self.movie_specs
         
