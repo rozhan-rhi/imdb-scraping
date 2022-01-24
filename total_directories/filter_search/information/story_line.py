7da0ba153266688fb7cc90c9410559c1a156b177
rozhan-rhi <rojanrahmani2002@yahoo.com>
2022-01-24 19:52:14 +0330

    edit some codes of previous files

--- total_directories/filter_search/information/story_line.py
+++ total_directories/filter_search/information/story_line.py
@@ -11,45 +11,34 @@ class Movie_Storyline(Filter_Base):
         self.p_link=p_link
     
     
+    
     def genre(self):
         # self.find_genre=self.pasre_url.find("li",{"data-testid":"storyline-genres"}) #finds genre part
         # self.genre_label=self.find_genre.span.text  #finds title(genre)
-        self.genre_content=[self.item.text for self.item in self.parse_url.find_all("a",href=True) if "search" in self.item['href'].split("/") and "?genres" in re.split('/|=',self.item['href'])]   #finds all genre types of movie
+        self.genre_content=[self.item.text for self.item in self.parse_url.find_all("a",href=True) if "/search/title/?genres" in self.item['href'].split("=")]   #finds all genre types of movie
         return self.genre_content
         
         
     def summary(self):
         try :
-            self.summary_part=self.parse_url.find("section",{"data-testid":"Storyline"}).find("div",class_="ipc-html-content ipc-html-content--base").div.text.strip()
+            self.summary_part=self.parse_url.find("div",class_="ipc-html-content ipc-html-content--base").text.strip()
             return [self.summary_part]
         except :pass
     
    
-    def movie_tag(self):
+    def movie_tag(self):       
+        self.tag_link=Link_Base(tagline=self.p_link).tagline
+        self.parse=super().parse_page(self.tag_link)
+        self.tag_part=self.parse.find("div",id="taglines_content")
+        self.all_tags=self.tag_part.find_all("div")
         try :
-            self.link_tag=self.parse_url.find("a",href=True) 
-            if "taglines" in self.link_tag['href'] :
-                self.line_names=["odd","even"]
-                for self.each_line_name in self.line_names :
-                    self.tagline_page=[self.tagline.text for self.tagline in self.parse_page(f"{'https://www.imdb.com'}{self.link_tag['href']}").find_all("div",class_=f"soda {self.each_line_name}")]
-                    return self.tagline_page
-        except:
-            self.tagline_page=self.parse_url.find("span",class_="ipc-metadata-list-item__list-content-item").text 
-            return self.tagline_page
-        finally :
-            return self.tagline_page
+            self.tag_content=list(set([self.each_part.text.replace(self.each_part.p.text,"").strip().replace("\n","") for self.each_part in self.all_tags]))  #it's for movies that has no tagline
             
-        
-        # self.tag_link=Link_Base(tagline=self.p_link).tagline
-        # self.parse=super().parse_page(self.tag_link)
-        # self.tag_part=self.parse.find("div",id="taglines_content")
-        # try :
-        #     self.all_tags=self.tag_part.find_all("div")
-        #     self.tag_content=[self.each_tag.text.strip().replace("\n","") for self.each_tag in self.all_tags]
-        # except :
-        #     self.tag_content="no tagline"
-        # finally :
-        #     return self.tag_content
+        except :
+            self.tag_content=[self.each_tag.text.strip().replace("\n","") for self.each_tag in self.all_tags if not "tagline" in self.each_tag.text and not "taglines" in self.each_tag.text]
+
+        finally :
+            return self.tag_content
         
         
     def movie_parents_guide(self):
