7c1f1e8ded758313c14096fd70e7d200022d3f84
Rozhin81 <79437690+Rozhin81@users.noreply.github.com>
2022-01-25 18:23:15 +0330

    new parts added

--- total_directories/filter_search/information/participant.py
+++ total_directories/filter_search/information/participant.py
@@ -9,6 +9,7 @@ class Movie_Participant(Filter_Base):
         
     def people(self):
         self.people_dict={}
+<<<<<<< Updated upstream
         self.info_parts=self.page.find_all("li",{"data-testid":"title-pc-principal-credit"}) #finds different parts of human table
         for self.each_one in self.info_parts:
             try: 
@@ -25,3 +26,25 @@ class Movie_Participant(Filter_Base):
                 self.content=self.all_options
                 self.people_dict[self.label_info]=self.content
         return self.people_dict
+=======
+        
+        self.info_parts=self.page.find_all("li",{"data-testid":"title-pc-principal-credit"}) #finds different parts of human table
+        for self.each_one in self.info_parts:
+            try :
+                self.info=self.each_one.find("a",class_="ipc-metadata-list-item__label ipc-metadata-list-item__label--link")
+                self.label_info=self.info.text 
+                self.see_full_page=self.parse_page(f"https://www.imdb.com{self.info['href']}").find("table",class_="cast_list")
+                self.all_options=[self.name_movie.text.strip() for self.name_movie in self.see_full_page.find_all("a",href=True) if "name" in self.name_movie['href'].split("/")]
+                self.content=self.all_options
+                self.people_dict[self.label_info]=self.content
+
+
+
+            except:
+                self.info=self.each_one.span
+                self.label_info=self.info.text
+                self.content=[self.each_name.text for self.each_name in self.info.parent.find_all("a",href=True) if "name" in self.each_name['href'].split("/")]
+                self.people_dict[self.label_info]=self.content
+        return self.people_dict
+
+>>>>>>> Stashed changes
---
+++ total_directories/filter_search/movies_based_information/first_page.py
@@ -0,0 +1,72 @@
+from bs4 import BeautifulSoup 
+from bs4.dammit import EncodingDetector
+import requests
+from collections import namedtuple
+from selenium import webdriver
+from selenium.webdriver.common.by import By 
+import time
+from .find_movies import Movies
+
+class What_To_Watch() :
+        
+    def parse_page(self,url) :
+        """parse the url and save html text"""
+        
+        self.url=url 
+        self.resp=requests.get(self.url)
+        self.parse=BeautifulSoup(self.resp.content,"html.parser")
+        return self.parse
+    
+       
+    
+    def guid(self) :
+        self.guid_dict={}
+        self.regex="ls([0-9]+)"
+        self.page=self.parse_page("https://www.imdb.com/what-to-watch/?ref_=hm_watch_btn")       
+        self.watch_guid=[self.item for self.item in self.page.find("div",class_="WatchGuides__ItemWrapper-sc-wu7yfb-1 fCITGH").find_all("div",class_="ipc-slate-card__title-text ipc-slate-card__title-text--clamp-none")]
+        self.obj_Movies=Movies()
+        for self.each_guid in self.watch_guid : 
+            self.guid_label=self.each_guid.text
+            self.guid_content=self.obj_Movies.several_movie(f"https://www.imdb.com{self.each_guid.parent['href']}")
+            self.guid_dict[self.guid_label]=self.guid_content
+
+       
+    def watch_parts(self) :
+        self.names_list=[]
+        
+        self.parser = 'html.parser' 
+        self.resp = requests.get("http://www.imdb.com/")
+        self.http_encoding = self.resp.encoding if 'charset' in self.resp.headers.get('content-type', '').lower() else None
+        self.html_encoding = EncodingDetector.find_declared_encoding(self.resp.content, is_html=True)
+        self.encoding = self.html_encoding or self.http_encoding
+        self.soup = BeautifulSoup(self.resp.content, self.parser, from_encoding=self.encoding)
+
+        #find 2 links 
+        self.WTO=[self.link['href'] for self.link in self.soup.find_all('a', href=True)  if "fan-favorites" in self.link['href'].split("/") or "popular" in self.link['href'].split("/") ]
+        
+        #create complete links
+        self.links=["{}{}".format(self.resp.url.replace('com/','com'),self.link) for self.link in self.WTO]
+
+        self.driver=webdriver.Firefox()        
+        for self.each_link in self.links :
+            self.driver.get(self.each_link)
+    
+            try :
+                #find the name of movies in these 2 links
+                self.movie_names=[self.name.text for self.name in self.driver.find_elements(By.CSS_SELECTOR ,'a[class="ipc-poster-card__title ipc-poster-card__title--clamp-2 ipc-poster-card__title--clickable"]')]
+                self.names_list.append(self.movie_names)
+            
+            except :
+                continue            
+            time.sleep(2)
+        self.driver.close()
+        
+        #creating namedtuple of tabs and movie_names
+        (self.tabs , self.movie_names )=["fan_favoratis","most_popular"] , self.names_list
+        self.WTW=namedtuple("WTW",self.tabs)
+        self.obj_=self.WTW(*self.movie_names)
+        return self.obj_._asdict()
+
+
+
+
